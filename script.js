26c2f6de81ac65f47b8f9681c5b82678a6d46134
Shreyas <ssshreyas34@gmail.com>
2021-03-04 18:42:57 +0530

    first

---
+++ script.js
@@ -0,0 +1,100 @@
+//var what=function(job,firstname)
+//{
+//    switch(job){
+//        case 'teacher':
+//            return firstname + ' teaches to code';
+//        case 'driver':
+//            return firstname + 'teaches to drive';
+//        default:
+//            return firstname + 'does something else';
+//    }
+//}
+//
+//console.log(what('teacher', 'Shreyas'));
+//var names=['John','Mark','Jane'];
+//
+//names[1]='Ben';
+//console.log(names);
+//
+//var john=['John','Smith',1990,'Coder'];
+//
+//var d=john.indexOf('designer')===-1?'John is not a designer':'John is a designer';
+//
+//console.log(d);
+//
+//var john={
+//    firstname:'John',
+//    lastname:'Smith',
+//    family:['Jake','Billy'],
+//    birthyear:200,
+//    calcAge: function()
+//    {
+//        this.age = 2020-this.birthyear;
+//    }
+//};
+//
+//john.calcAge();
+//console.log(john);
+//console.log(john);
+
+
+//var john={
+//    fullname:'Shreyas',
+//    mass:60,
+//    height:1.59,
+//    calcBmi:function()
+//    {
+//        this.bmi=this.mass/(this.height*this.height);
+//        return this.bmi;
+//    }
+//}
+
+
+//var mark={
+//    fullname:'Shreyas',
+//    mass:70,
+//    height:1.69,
+//    calcBmi:function()
+//    {
+//        this.bmi=this.mass/(this.height*this.height);
+//        return this.bmi;
+//    }
+//}
+//
+//john.calcBmi();
+//mark.calcBmi();
+//console.log(john,mark);
+
+//var john=['John','Smith',1990,'Coder'];
+//for(var i=0;i<4;i++)
+//    {
+//        console.log(john[i]);
+//    }
+
+//var age=20;
+//function year()
+//{
+//    var age=19;
+//    console.log(2000-age);
+//}
+//year();
+//console.log(age);
+
+//var a='Hello';
+//first();
+//function first()
+//{
+//    var b='Hi';
+//    second();
+//function second()
+//{
+//    var c='Hey';
+//    console.log(a+b+c);
+//}
+//}
+
+//console.log(this);
+
+//var d=Math.floor(Math.random()*100);
+//console.log(d);
+
